f2e477bc56ec3fac5a2856fd686e96215081fa09
Ayushi Patra <36570209+ayushipatra369@users.noreply.github.com>
2019-06-02 23:01:31 -0400

    Update code.py

--- Probability-of-the-Loan-Defaulters/code.py
+++ Probability-of-the-Loan-Defaulters/code.py
@@ -1,78 +1,89 @@
-# --------------
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from fractions import Fraction
 
 # code starts here
 
 df = pd.read_csv(path)
-# print(df.head())
 total = len(df)
+
+# probability of  fico score greater than 700
 
 p_a = len(df[df['fico']>=700])/total
 print(p_a) 
+
+# probability of purpose == debt_consolidation
+
 p_b = len(df[df['purpose']=='debt_consolidation'])/total
+print(p_b)
+
+# Create new dataframe for condition ['purpose']== 'debt_consolidation' 
+
 df1 = df['purpose']=='debt_consolidation'
 
-p_a_b = len(df.groupby('purpose')['fico'].value_counts())/p_b 
+# Calculate the P(A|B)
+
+p_a_b = len(df.groupby('purpose')['fico'].value_counts())/p_a 
 print(p_a_b) 
+
+# Check whether the P(A) and P(B) are independent from each other
+
 result = p_a_b == p_a
 print(result) 
-# print(p_b_a == p_a)
-
-# code ends here
 
 
-# --------------
-# code starts here
-from fractions import Fraction
+# probability of paid_back_loan is Yes
 prob_lp = len(df[df['paid.back.loan'] == 'Yes'])/total
 print(prob_lp) 
 
-
+# create new dataframe for paid.back.loan == 'Yes'
 new_df = df['paid.back.loan'] == 'Yes' 
-# print(new_df) 
 
+# probability of the credit policy is Yes
 prob_cs = len(df[df['credit.policy'] == 'Yes'])/total 
 print(prob_cs)
+
+# Calculate the P(B|A)
 
 a = df.groupby('paid.back.loan')['credit.policy'].value_counts()/df.groupby('paid.back.loan')['credit.policy'].count()
 print(a) 
 
-prob_pd_cs = 0.832318
+prob_pd_cs = a.max()
 
-bayes = 0.868482 
+# bayes theorem 
+bayes = (prob_pd_cs * prob_lp) / prob_cs 
 print(bayes) 
 
-# code ends here 
-
-
-# --------------
-# code starts here
-import matplotlib.pyplot as plt
-
+# Visualize 
+# create bar plot for purpose
 ax = df['purpose'].value_counts().plot.bar(rot=45) 
+plt.title('Probability Distribution of Purpose')
+plt.ylabel('Probability')
+plt.xlabel('Number of Purpose')
+plt.show()
 
-
+#create new dataframe for paid.back.loan == 'No'
 df1 = df[df['paid.back.loan'] == 'No']
-# ax = df.plot()
+
+# Plotting two DataFrame
+
 df1.plot(ax=ax)
-# df.plot(x=df['purpose'].value_counts(), y=["A", "B", "C"], kind="bar")
+df1.purpose.value_counts(normalize=True).plot(kind='bar')
+plt.title('Probability Distribution of Purpose')
+plt.ylabel('Probability')
+plt.xlabel('Number of Purpose')
+plt.show()
 
-
-
-
-# code ends here
-
-
-# --------------
-# code starts here
+# Plot the histogram for visualization of the continuous variable
+# Calculate median 
 inst_median = df['installment'].median()
 inst_mean = df['installment'].mean()
-df['installment'].plot.hist()
-df['log.annual.inc'].plot.hist() 
 
+# histogram for installment
+df['installment'].plot.hist(normed=True)
+plt.show()
 
-# code ends here
-
-
+#histogram for log anual income
+df['log.annual.inc'].plot.hist(normed=True) 
+plt.show() 
